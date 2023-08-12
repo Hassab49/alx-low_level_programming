38e1dbe4f73401504ad771672ab01cf702aeedc5
Hassab49 <ahmad.nada00@gmail.com>
2023-08-11 19:22:01 -0700

    main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Enery point code 
+ * main - Entry point
+ *
+ * Descreption : c program that print with put funaction
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }    
